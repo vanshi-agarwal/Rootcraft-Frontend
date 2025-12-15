911995159cfd54a3f090a9233ecae628f870cebe
vanshi-agarwal <158465495+vanshi-agarwal@users.noreply.github.com>
2025-12-15 22:50:04 +0530

    cors issue

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_URL =
-    process.env.NEXT_BACKEND_URL;
+    process.env.NEXT_PUBLIC_BACKEND_URL;
 
 const api = axios.create({
     baseURL: `${API_URL}/api`,
